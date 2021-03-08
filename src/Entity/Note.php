b4f9807ae66323cd39a7c106a17c9f6f1eb1186f
Gilles Avraam <gillouavraam@gmail.com>
2021-03-08 21:47:54 +0100

    [add]: Api platform

--- src/Entity/Note.php
+++ src/Entity/Note.php
@@ -2,11 +2,13 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\NoteRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity(repositoryClass=NoteRepository::class)
+ * @ApiResource
  */
 class Note
 {
